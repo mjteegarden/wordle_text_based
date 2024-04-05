eb24eab86acdbb5cd32306c1baf317d04764cb18
mjteegarden <133513994+mjteegarden@users.noreply.github.com>
2024-04-04 20:02:39 -0500

    Update wordle-text-based-v1.py

--- wordle-text-based-v1.py
+++ wordle-text-based-v1.py
@@ -3,21 +3,24 @@ def instructions():
           A player must guess a five letter hidden word.
           You have six (6) attempts to guess the hidden word.
           Your progress is shown by your progress guide.
-          "/" indicates the letter at that position was guessed correctly.
-          "+" indicates the letter at that position is in the hidden word, but is in a different position.
-          "x" indicates that the letter at that position is wrong and is not in the hidden word.""")
+          " / " indicates the letter at that position was guessed correctly.
+          " + " indicates the letter at that position is in the hidden word, but is in a different position.
+          " x " indicates that the letter at that position is wrong and is not in the hidden word.""")
 
 instructions()
 
 def check_word():
-    #    TO-DO ... print used letters for each attempt, so player can see which letters are / (right letter in right place), + (right letter but wrong place), and x (wrong letter; not in word)
-    #    TO-DO ... add feature, randomly choose word from text list
-    #    TO-DO ... track words used from text list; don't choose those words until entire list is used
+    #    TO-DO 1 ... print used letters for each attempt, so player can see which letters are / (right letter in right place), + (right letter but wrong place), and x (wrong letter; not in word)
+    #    TO-DO 2 ... add feature, randomly choose word from text list
+    #    TO-DO 3 ... track words used from text list; don't choose those words until entire list is used
     hidden_word = "crate"
     attempts = 6
 
     while attempts > 0:
+        print()
         guess = str(input("Guess the word: "))
+        #  TO-DO 4 ... error check to only accept 5 letter words
+        #  TO-DO 5 ... check guessword against text list (see above TO-DO 2)
         if guess == hidden_word:
             print("You guessed the word correctly!  Congratulations!")
             print()
